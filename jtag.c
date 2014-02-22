c24fc63ad749d60fa9fbbd08d364bfe5a6d816cc
Michael Davidsaver <mdavidsaver@bnl.gov>
2014-02-22 17:08:02 -0500

    minor

--- jtag.c
+++ jtag.c
@@ -1,28 +1,32 @@
 
 enum jtags {
   jtag_reset,
-  jtag_idle,
-  jtag_dr_scan,
+  jtag_idle,     // 1
+  jtag_dr_scan,  // 2
   jtag_dr_cap,
   jtag_dr_shift,
   jtag_dr_exit1,
   jtag_dr_pause,
   jtag_dr_exit2,
-  jtag_dr_update,
-  jtag_ir_scan,
+  jtag_dr_update, // 8
+  jtag_ir_scan,   // 9
   jtag_ir_cap,
   jtag_ir_shift,
   jtag_ir_exit1,
   jtag_ir_pause,
   jtag_ir_exit2,
-  jtag_ir_update,
+  jtag_ir_update,// f
 };
 
 static
 void jtag_sim(struct ppsdev *port)
 {
+    int trace = 0;
     static enum jtags state = jtag_reset;
+    enum jtags prev_state = state;
     static int prev_tck = 0;
+
+    static u64 hist_tms = 0, hist_tdo = 0, hist_tdi = 0;
 
     static const u32 myidcode = 0x89BEEF01;
 
@@ -64,7 +68,7 @@ void jtag_sim(struct ppsdev *port)
     case jtag_dr_shift:
         port->tdo = *DR&1;
         *DR >>= 1;
-        if(port->tdi) *DR |= (1<<DR_len);
+        if(port->tdi) *DR |= (1<<(DR_len-1));
         if(port->tms==1) state=jtag_dr_exit1;
         break;
 
@@ -76,7 +80,6 @@ void jtag_sim(struct ppsdev *port)
 
     case jtag_dr_update:
         if(port->tms==0) state=jtag_idle; else state=jtag_dr_scan;
-        printk(KERN_INFO "DR  %04x\n", *DR);
         break;
 
 
@@ -112,8 +115,20 @@ void jtag_sim(struct ppsdev *port)
 
         default: DR=&DR_UNKNOWN; *DR = 0xdeadbeef; DR_len = 32; break;
         }
-        printk(KERN_INFO "IR %04x\n", IR);
         if(port->tms==0) state=jtag_idle; else state=jtag_dr_scan;
         break;
     }
+
+    trace = prev_state != state;
+
+    hist_tms<<=1; hist_tdo<<=1; hist_tdi<<=1;
+    if(port->tms) hist_tms |= 1;
+    if(port->tdi) hist_tdi |= 1;
+    if(port->tdo) hist_tdo |= 1;
+    if(trace)
+        printk(KERN_INFO "%2x -> %2x IR %04x DR %08x TMS %016llx TDI %016llx TDO %016llx\n",
+               (int)prev_state, (int)state, IR, *DR,
+               (unsigned long long) hist_tms,
+               (unsigned long long) hist_tdi,
+               (unsigned long long) hist_tdo);
 }
